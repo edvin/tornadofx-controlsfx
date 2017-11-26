2cbe8d5071b8586f8784753942be1b7ecf0df686
Leonid <sleonidy@gmail.com>
2017-11-26 14:53:24 +0200

    Update statusbar test app

--- src/test/kotlin/tornadofx/controlsfx/testapps/StatusBarTest.kt
+++ src/test/kotlin/tornadofx/controlsfx/testapps/StatusBarTest.kt
@@ -2,11 +2,14 @@ package tornadofx.controlsfx.testapps
 
 import javafx.geometry.Insets
 import javafx.geometry.Orientation
-import tornadofx.*
-import javafx.scene.layout.CornerRadii
-import javafx.scene.layout.BackgroundFill
+import javafx.scene.control.Button
 import javafx.scene.layout.Background
+import javafx.scene.layout.BackgroundFill
+import javafx.scene.layout.CornerRadii
 import javafx.scene.paint.Color
+import tornadofx.*
+import tornadofx.controlsfx.left
+import tornadofx.controlsfx.right
 import tornadofx.controlsfx.statusbar
 
 
@@ -27,14 +30,19 @@ class StatusBarView : View("Status Bar View") {
             }
         }
         statusbar("") {
-            button("1") {
-                background = Background(BackgroundFill(Color.ORANGE, CornerRadii(2.0), Insets(4.0)))
+            right {
+                button("1") {
+                    background = Background(BackgroundFill(Color.ORANGE, CornerRadii(2.0), Insets(4.0)))
+                }
             }
+
             separator(Orientation.VERTICAL) {
 
             }
-            button("2") {
-                background = Background(BackgroundFill(Color.ORANGE, CornerRadii(2.0), Insets(4.0)))
+            left {
+                button("2") {
+                    background = Background(BackgroundFill(Color.ORANGE, CornerRadii(2.0), Insets(4.0)))
+                }
             }
         }
     }
