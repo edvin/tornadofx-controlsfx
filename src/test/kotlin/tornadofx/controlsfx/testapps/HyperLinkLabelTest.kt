30b5d80991d1c8127b253fee79184abfb25dc3e5
Leonid <sleonidy@gmail.com>
2017-12-12 22:31:52 +0200

    Added test for hyperlinklabel with command

--- src/test/kotlin/tornadofx/controlsfx/testapps/HyperLinkLabelTest.kt
+++ src/test/kotlin/tornadofx/controlsfx/testapps/HyperLinkLabelTest.kt
@@ -2,11 +2,20 @@ package tornadofx.controlsfx.testapps
 
 import tornadofx.*
 import tornadofx.controlsfx.action
+import tornadofx.controlsfx.command
 import tornadofx.controlsfx.hyperlinklabel
 
 class HyperLinkLabelApp : App(HyperLinkLabelView::class)
+class HyperLinkLabelViewModel : ViewModel() {
+    val hyperlinkCommand = command(this::hyperlinkAction)
+
+    private fun hyperlinkAction(text:String) {
+        log.info { "link $text clicked" }
+    }
+}
 
 class HyperLinkLabelView : View("HyperLinkLabel") {
+    val viewModel by inject<HyperLinkLabelViewModel>()
     override val root = vbox {
         hyperlinklabel("This is [text] link. Another [link]") {
             action {
@@ -19,6 +28,9 @@ class HyperLinkLabelView : View("HyperLinkLabel") {
                     }
                 }
             }
+        }
+        hyperlinklabel("This is [text] link with command. And another [link]") {
+            command = viewModel.hyperlinkCommand
         }
     }
 
